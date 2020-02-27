25465112c03a360556c0a6eb922566653860b0d2
kiko <isabeki@au1.ibm.com>
2020-02-27 13:00:39 +1100

    setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# nutsgen
+nuts ml/flow functions for genomics
+
+__install using pip for python 3:__
+
+`pip3 install git+ssh://git@github.ibm.com/aur-genomics/pygen.git`
+
+__install from a custom branch:__
+
+`pip3 install git+ssh://git@github.ibm.com/aur-genomics/pygen.git@<branch>`
+
+
+__usage:__
+
+import pygen
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='nutsgen',
+    version='0.0.1',
+    description='genomics processing and analysis',
+    url='https://github.ibm.com/isabeki/pylinklearn.git',
+    author='Isabell Kiral',
+    author_email='isa.kiral@gmail.com',
+    license='unlicense',
+    packages=['nutsgen'],
+    zip_safe=False,
+    install_requires=[
+        'nutsflow',
+        'nutsml',
+	    'pandas-plink',
+        'numpy',
+        'pandas'
+    ],
+)
