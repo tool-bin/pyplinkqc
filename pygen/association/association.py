7eddda5c15f4e0cbfaca6c8f4f1522c4645af90b
Nathalie Willems <nathalie.willems@ibm.com>
2020-05-27 16:36:34 +1000

    added support for lin/log association testing

--- pygen/association/association.py
+++ pygen/association/association.py
@@ -1,13 +1,5 @@
 import pandas as pd
 import os
-import pickle
-
-def read_bfile(binaryfile: str):
-    with open(binaryfile, 'rb') as file:
-        tmplist = pickle.load(file)
-    filelist = [str(i) for i in tmplist]
-    return filelist
-
 
 def generate_pheno_plink(bfile: str, phenofile: str, outfile: str):
     """Generates plink binary files annotated with case-control phenotypes.
@@ -25,27 +17,45 @@ def generate_pheno_plink(bfile: str, phenofile: str, outfile: str):
     os.system(command)
 
 
+def perform_simple_assoc(bfile: str, adjust: bool=False, outfile: str):
+    """Performs 1 df chi-squared allelic assocation test. Assumes the plink binary files contain phenotype annotations (6th column of the .fam/.ped file). PLINK automatically infers qualitative vs quantitative (contains values other than 0, 1, 2 or missing) phenotypes.
 
-def perform_simple_assoc(bfile: str, phenofile: str, outfile: str):
-    command = f"./plink --bfile {bfile} --assoc --out {outfile}"
+    Key arguments:
+    --------------
+    bfile: str
+        prefix for plink binary files (.bed, .bim, .fam)
+    adjust: bool
+        optional parameter specifying whether to employ Bonferroni Correction for adjustment of the p-values
+    outfile: str
+        prefix for the output plink binary files
+    """
+    if adjust:
+        command = "./plink --bfile {} --adjust --assoc --out {}".format(bfile, outfile)
+    else:
+        command = "./plink --bfile {} --assoc --out {}".format(bfile, outfile)
     os.system(command)
 
-def perform_adjust_assoc(bfile: str, phenofile: str, outfile: str):
-    command = f"./plink --bfile {bfile} --make-pheno {phenofile} '*' --assoc --adjust --out {outfile}"
+
+def perform_cov_assoc(bfile: str, type: str="linear", cov: str="", outfile: str):
+    """Performs linear/logistic regression association analysis.
+
+    Key arguments:
+    --------------
+    bfile: str
+        prefix for plink binary files (.bed, .bim, .fam)
+    type: str
+        specifies whether to run a "linear" or "logistic" regression
+    cov: str
+        optional parameter that specifies a covariate file (e.g txt file listing a covariate)
+    outfile: str
+        prefix for the output plink binary files
+    """
+    if type=="linear":
+        command = "./plink --bfile {} --linear --out {}".format(bfile, outfile)
+    elif type=="log":
+        command = "./plink --bfile {} --log --out {}".format(bfile, outfile)
+    else:
+        raise Exception("{} is not a supported regression model. Please try linear or log".format(type))
+    if cov != "":
+        command = command + " --cov {}".format(cov)
     os.system(command)
-
-
-#plink --bfile mydata --make-pheno p1.list * --assoc
-
-#%%
-
-bfile="HapMap_3_r3_1"
-eidfile = "../../../modellingScripts/nat/cohortPipeline/merged_gpclincal_maindataset_eids_borderline glaucoma.txt"
-caselist = generate_phenofile(bfile=bfile, eidfile=eidfile, outfile="caselist.csv")
-
-#%%
-
-perform_assoc(bfile=bfile, phenofile="caselist.csv", outfile="test_assoc")
-
-
-/Users/nathaliewillems/Box/Projects/Genomics/data/UKBB_Data/main_data/ukb41268_head100.csv
